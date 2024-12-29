3f6f01782ff621b18c6b6fb48217665f036028e1
서민재 <hey_minj@naver.com>
2024-12-29 19:05:08 +0900

    3-2 페이징 기능

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,10 +1,16 @@
 package com.mysite.sbb.question;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import com.mysite.sbb.DataNotFoundException;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,9 +19,16 @@ import java.util.Optional;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList(){
-        return this.questionRepository.findAll();
+//    public List<Question> getList(){
+//        return this.questionRepository.findAll();
+//
+//    }
 
+    public Page<Question> getList(int page){
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate")); // 정렬 조건 추가하기
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 
     public Question getQuestion(Integer id){
