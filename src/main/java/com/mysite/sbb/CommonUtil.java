8f39d33babd6943f6c19663dab043044f84b6156
서민재 <hey_minj@naver.com>
2025-01-10 21:12:10 +0900

    3-12 마크다운

---
+++ src/main/java/com/mysite/sbb/CommonUtil.java
@@ -0,0 +1,19 @@
+package com.mysite.sbb;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+import javax.swing.text.html.HTMLDocument;
+
+@Component
+public class CommonUtil {
+    public String markdown(String markdown){
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+
+}
